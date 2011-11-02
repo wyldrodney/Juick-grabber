33a1e7d348214176ca2ff76ad810c2b093c34e0c
Dmitry <wyldrodney@gmail.com>
2011-11-02 20:47:09 +1100

    Works

--- driver.rb
+++ driver.rb
@@ -7,7 +7,7 @@ password = 'Lf,k_"ynshghfqp'
 #ARGV[1]
 
 
-system "rm juick-cookie"
+system "rm -f juick-cookie"
 
 system "curl -s -d 'nick=#{login}' -d 'passwd=#{password}' -c juick-cookie http://juick.com/login"
 
@@ -19,7 +19,10 @@ else
 end
 
 
-@nicks = @tags = @texts = @nums = []
+@nicks = []
+@tags = []
+@texts = []
+@nums = []
 
 
 def parse(page)
@@ -27,17 +30,19 @@ def parse(page)
 	filename = '/tmp/juick-parser-' + srand.to_s
 
 	system 'curl -s -b juick-cookie -o ' + filename + ' http://juick.com/?show=my&page=' + page.to_s
-	sleep(2)
+
+	until system "ls #{filename}" do
+		sleep(1)
+	end
 
 	source = Nokogiri::HTML(open(filename), nil, 'UTF-8')
-	system "rm #{filename}"
+	system "rm -f #{filename}"
 
 	source.search("#content .liav .msg").each do |message|
 
 		puts "Message..."
 
 		@nicks << message.search("big a[1]").children.to_s
-
 
 		tag_hash = message.search("big a")
 
@@ -65,5 +70,4 @@ end
 
 parse(11)
 
-#puts @nicks
-
+puts @nicks
